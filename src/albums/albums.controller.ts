53a9e5bf0360ced7fe6465e26bbbdaf2cdb90f04
Sadykov Arsen <LoCo10291998@gmail.com>
2025-05-23 15:27:45 +0600

    Made saving Image

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Album, AlbumDocument } from '../schemas/album.schema';
 import { Request } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { CreateAlbumDto } from './create-album.dto';
 import { Artist } from '../schemas/artist.schema';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('albums')
 export class AlbumsController {
@@ -31,12 +40,18 @@ export class AlbumsController {
   }
 
   @Post()
-  async create(@Body() albumDto: CreateAlbumDto) {
+  @UseInterceptors(
+    FileInterceptor('image', { dest: './public/uploads/albums' }),
+  )
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() albumDto: CreateAlbumDto,
+  ) {
     const album = new this.albumModel({
       artist: albumDto.artist,
       title: albumDto.title,
       date: albumDto.date,
-      image: albumDto.image,
+      image: file ? '/uploads/albums/' + file.filename : null,
     });
 
     return await album.save();
--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -11,6 +11,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Artist, ArtistDocument } from '../schemas/artist.schema';
 import { CreateArtist } from './create-artist.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('artists')
 export class ArtistsController {
@@ -27,14 +28,16 @@ export class ArtistsController {
     return this.artistModel.find({ _id: id });
   }
   @Post()
-  @UseInterceptors('image', { dest: './public/uploads/artists' })
+  @UseInterceptors(
+    FileInterceptor('image', { dest: './public/uploads/artists' }),
+  )
   create(
     @UploadedFile() file: Express.Multer.File,
     @Body() artistDto: CreateArtist,
   ) {
     const artist = new this.artistModel({
       name: artistDto.name,
-      image: artistDto.image,
+      image: file ? '/uploads/artists/' + file.filename : null,
       description: artistDto.description,
     });
     return artist.save();
